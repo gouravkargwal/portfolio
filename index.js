75dae5b3bcacfbf0f16514344bb38d829be275c2
Gourav Kargwal <72146455+gouravkargwal@users.noreply.github.com>
2021-01-28 20:09:29 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = "gray";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+});
+
+const date = new Date();
+document.querySelector("#year").innerHTML = date.getFullYear();
